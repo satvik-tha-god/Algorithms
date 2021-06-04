61c0abd835b4f9d2735a9f6bc977ced5c0c8dad8
satvik-tha-god <satviksinghx@gmail.com>
2021-06-04 23:00:09 +0530

    :/

---
+++ linkedLists/Set-1/Easy/intersectionLL.cpp
@@ -0,0 +1,20 @@
+// problem lunk:https://leetcode.com/problems/intersection-of-two-linked-lists/submissions/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *ptrA = headA, *ptrB = headB;
+while (ptrA != ptrB) {
+ptrA = ptrA ? ptrA->next : headB;
+ptrB = ptrB ? ptrB->next : headA;
+}
+return ptrA;
+    }
+};
