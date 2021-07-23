b29b0d4b2025d93f883b2c10361e0fa30fa0d1d7
satvik-tha-god <satviksinghx@gmail.com>
2021-07-23 17:09:09 +0530

    ez

---
+++ Pattern/rhombus-pattern.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int rows;
+    cout<<"Enter the number of rows and columns"<<endl;
+    cin>>rows;
+    for(int i=0;i<rows;i++){
+        for(int k=0;k<=i-1;k++){
+            cout<<" ";
+        }
+        for(int j=0;j<rows;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
