9d7386dec1c05aeab95e992068faf0ddfc522fb1
satvik-tha-god <satviksinghx@gmail.com>
2021-09-17 00:52:08 +0530

    Got an inkling on idea

--- Leetcode/linkedLists/Set-1/Medium/rotateLL.cpp
+++ Leetcode/linkedLists/Set-1/Medium/rotateLL.cpp
@@ -9,23 +9,22 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
-public:
-    ListNode* rotateRight(ListNode* head, int k) {
-        if(head==NULL) return head;
-        int n=1;
-        ListNode* temp=head;
-        while(temp->next){
-            n++;
-            temp=temp->next;
-        }
-        temp->next=head;
-        temp=head;
-        for(int i=1;i<n-k%n;i++)
-            temp=temp->next;
-        head=temp->next;
-        temp->next=NULL;
-
-        return head;
-    }
-};
+ class Solution {
+ public:
+     ListNode* rotateRight(ListNode* head, int k) {
+         if(head==NULL) return head;
+         int n=1;
+         ListNode* temp=head;
+         while(temp->next){
+             n++;
+             temp=temp->next;
+         }
+         temp->next=head;
+         temp=head;
+         for(int i=1; i<n-k%n; i++)
+             temp=temp->next;
+         head=temp->next;
+         temp->next=NULL;
+         return head;
+     }
+ };
