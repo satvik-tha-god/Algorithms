398bdb5b27c70d3b64df2119657b3139eba8ba4f
satvik-tha-god <satviksinghx@gmail.com>
2021-09-16 23:46:27 +0530

    Why am I so dumb

--- Leetcode/linkedLists/Set-1/Easy/removeDuplicateLL.cpp
+++ Leetcode/linkedLists/Set-1/Easy/removeDuplicateLL.cpp
@@ -1,23 +1,15 @@
-// problem link:https://practice.geeksforgeeks.org/problems/remove-duplicate-element-from-sorted-linked-list/1
-void removeDuplicates(struct Node* head)
-{
-    void removeDuplicates(struct Node* head)
-{
-struct Node* temp = head;
-struct Node* prev=head;
-while (temp != NULL)
-{
-if(temp->data != prev->data)
-{
-prev->next = temp;
-prev = temp;
-}
-temp = temp->next;
-}
-if(prev != temp)
-{
-prev->next = NULL;
-}
-}
+void removeDuplicates(struct Node* head) {
+    struct Node* temp=head;
+    struct Node* prev=head;
+    while(temp!=NULL){
+        if(temp->data != prev->data){
+            prev->next = temp;
+            prev = temp;
+        }
+        temp=temp->next;
+        if(prev!= temp){
+            prev->next=NULL;
+        }
+    }
 
 }
