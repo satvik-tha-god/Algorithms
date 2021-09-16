952155ef33133be121096ccec03abc8a75b274b0
satvik-tha-god <satviksinghx@gmail.com>
2021-09-16 23:36:34 +0530

    Useful

--- Leetcode/linkedLists/Set-1/Easy/reverseLL.cpp
+++ Leetcode/linkedLists/Set-1/Easy/reverseLL.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-ListNode* reverseList(ListNode* head) {
-ListNode *curr,*nextnode,*prev;
-if(!head) return head; //empty list
-curr=head;
-prev=NULL;
-nextnode=curr->next;
-while(curr)
-{
-curr->next=prev;
-prev=curr;
-curr=nextnode;
-if(nextnode) nextnode=curr->next;
-}
-return prev;
-}
+    ListNode* reverseList(ListNode* head) {
+        ListNode *curr,*nextnode,*prev;
+        if(!head) return head;
+        curr=head;
+        prev=NULL;
+        nextnode=curr->next;
+
+        while(curr){
+            curr->next=prev;
+            prev=curr;
+            curr=nextnode;
+            if(nextnode) nextnode=curr->next;
+        }
+
+        return prev;
+    }
 };
