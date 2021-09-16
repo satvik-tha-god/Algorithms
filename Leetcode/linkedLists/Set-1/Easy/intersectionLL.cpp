bea0ba783a8a12005cd6466b22f0f60c237c3a40
satvik-tha-god <satviksinghx@gmail.com>
2021-09-17 00:32:28 +0530

    Highly interesting and important

--- Leetcode/linkedLists/Set-1/Easy/intersectionLL.cpp
+++ Leetcode/linkedLists/Set-1/Easy/intersectionLL.cpp
@@ -18,3 +18,49 @@ ptrB = ptrB ? ptrB->next : headA;
 return ptrA;
     }
 };
+
+//Alternative way
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int lenA=0,lenB=0;
+        ListNode* tempA = headA;
+        ListNode* tempB = headB;
+        while(tempA!=NULL){
+            tempA=tempA->next;
+            lenA++;
+        }
+        while(tempB!=NULL){
+            tempB=tempB->next;
+            lenB++;
+        }
+        int d;
+        ListNode* ptr1;
+        ListNode* ptr2;
+        if(lenA>lenB){
+            d=lenA-lenB;
+            ptr1 = headA;
+            ptr2 = headB;
+        }
+        else{
+            d=lenB-lenA;
+            ptr1 = headB;
+            ptr2 = headA;
+        }
+        while(d){
+            ptr1=ptr1->next;
+            if(ptr1 == NULL){
+                return NULL;
+            }
+            d--;
+        }
+        while(ptr1 != NULL && ptr2!= NULL){
+            if(ptr1 == ptr2){
+                return ptr1;
+            }
+            ptr1=ptr1->next;
+            ptr2=ptr2->next;
+        }
+        return NULL;
+    }
+};
--- Leetcode/linkedLists/Set-1/Medium/oddEvenLL.cpp
+++ Leetcode/linkedLists/Set-1/Medium/oddEvenLL.cpp
@@ -9,19 +9,19 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
-public:
-    ListNode* oddEvenList(ListNode* head) {
-    if(!head) return head;
-    ListNode *odd=head, *evenhead=head->next, *even = evenhead;
-    while(even && even->next)
-    {
-    odd->next = odd->next->next;
-    even->next = even->next->next;
-    odd = odd->next;
-    even = even->next;
-    }
-    odd->next = evenhead;
-    return head;
-    }
-};
+ class Solution {
+ public:
+     ListNode* oddEvenList(ListNode* head) {
+         if(!head) return head;
+         ListNode *odd=head,*evenhead=head->next,*even=evenhead;
+         while(even && even->next){
+             odd->next = odd->next->next;
+             even->next = even->next->next;
+             odd = odd->next;
+             even = even->next;
+         }
+         odd->next = evenhead;
+
+         return head;
+     }
+ };
