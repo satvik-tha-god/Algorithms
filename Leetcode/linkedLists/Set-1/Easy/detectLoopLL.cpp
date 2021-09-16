618fb1f17f2511836d7e8b3ab1d0efb7d728f6c2
satvik-tha-god <satviksinghx@gmail.com>
2021-09-16 23:37:54 +0530

    Hare and Tortoise algorithm

--- Leetcode/linkedLists/Set-1/Easy/detectLoopLL.cpp
+++ Leetcode/linkedLists/Set-1/Easy/detectLoopLL.cpp
@@ -5,8 +5,8 @@ class Solution
     //Function to check if the linked list has a loop.
     bool detectLoop(Node* head)
     {
-         Node *slow_p = head, *fast_p = head;
-         while (slow_p && fast_p && fast_p->next) {
+        Node *slow_p = head, *fast_p = head;
+        while (slow_p && fast_p && fast_p->next) {
         slow_p = slow_p->next;
         fast_p = fast_p->next->next;
         if (slow_p == fast_p) {
